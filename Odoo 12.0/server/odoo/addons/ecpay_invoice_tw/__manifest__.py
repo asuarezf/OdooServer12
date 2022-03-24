5cb2e6165a87334d49f96bbb2fbf58123d84d0d6
Andres Suarez <12-10925@usb.ve>
2022-03-24 09:20:19 +0800

    Ecpayment Upload

---
+++ Odoo 12.0/server/odoo/addons/ecpay_invoice_tw/__manifest__.py
@@ -0,0 +1,35 @@
+{
+    'name': 'ECPay 綠界第三方電子發票模組',
+    'version': '1.5',
+    'category': 'Accounting',
+    'author': 'ECPAY',
+    'website': 'http://www.ecpay.com.tw',
+    'description': """
+        綠界 台灣電子發票Odoo模組\n
+        使用 pyhton3\n
+        需要依賴模組 ecpay_invoice3\n
+        sudo pip3 install ecpay_invoice3
+    """,
+    'summary': '電子發票 (Invoice): ECPay 綠界第三方電子發票模組',
+    'depends': ['account', 'sale','sale_management', 'website_sale'],
+    'data': [
+        'views/res_config_setting_view.xml',
+        'data/demo.xml',
+        'views/sale_order_view.xml',
+        'views/website_sale.xml',
+        'views/account_invoice_view.xml',
+        'views/uniform_invoice_view.xml',
+        'views/menu.xml',
+        'report/uniform_invoice_report.xml',
+        'views/account_invoice_refund_view.xml',
+        'security/ir.model.access.csv',
+    ],
+    'external_dependencies': {
+        'python': [
+            'ecpay_invoice'
+        ],
+        'bin': []
+    },  
+    'installable': True,
+    'application': True,
+}
